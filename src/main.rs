aceb10c97032cd9c4f94c5ad0561c21e527561ee
Mike Perry <mike@mikeperry.io>
2019-01-27 15:27:33 -0500

    initial commit; drawing paddles

---
+++ src/main.rs
@@ -0,0 +1,40 @@
+extern crate amethyst;
+mod pong;
+
+use crate::pong::Pong;
+use amethyst::prelude::*;
+use amethyst::core::transform::TransformBundle;
+use amethyst::renderer::{DisplayConfig, DrawFlat2D, Event, Pipeline,
+                         RenderBundle, Stage, VirtualKeyCode};
+
+fn main() -> amethyst::Result<()> {
+
+    amethyst::start_logger(Default::default());
+    // We'll put the rest of the code here.
+
+    use amethyst::utils::application_root_dir;
+
+    let path = format!("{}/resources/display_config.ron", application_root_dir());
+
+    let config = DisplayConfig::load(&path);
+
+    let pipe = Pipeline::build()
+    .with_stage(
+        Stage::with_backbuffer()
+            .clear_target([0.0, 0.0, 0.0, 1.0], 1.0)
+            .with_pass(DrawFlat2D::new()),
+    );
+
+    let game_data = GameDataBuilder::default()
+        .with_bundle(
+            RenderBundle::new(pipe, Some(config))
+            .with_sprite_sheet_processor()
+            )?
+            .with_bundle(TransformBundle::new())?;
+
+    let mut game = Application::new("./", Pong, game_data)?;
+
+    game.run();
+
+    Ok(())
+}
